7fb32003001894cfe713cdb98fe0dff1c0a9d6af
Emmanuel B <blancocemmanuel@gmail.com>
2026-02-25 21:13:05 +1100

    fifth commit

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,6 +1,16 @@
 import React from "react";
 
 export const TileList = () => {
+
+  
+
+  /*contacts.map((contact, index) => {
+  const { name, ...description } = contact;
+  return <Tile key={index} name={name} description={description} />;
+   });*/
+
+
+
   return (
     <div>
       
